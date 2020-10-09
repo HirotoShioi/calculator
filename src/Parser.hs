33300f704b7ce11414fa760f3c889d9171db7291
Hiroto Shioi <shioihigg@gmail.com>
2020-10-09 14:26:16 +0900

    Explicit import

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,7 +1,9 @@
 module Parser where
 
-import Text.Parsec
-import Text.Read (readMaybe)
+import           Text.Parsec (ParseError, Parsec, anyChar, choice, digit,
+                              getState, many1, modifyState, oneOf, optionMaybe,
+                              parserFail, runParser, spaces, (<?>), (<|>))
+import           Text.Read   (readMaybe)
 
 -- 式5 + 6 * (7 + 8) を [Num 5, Num 6, Multiply, OpenBracket, Num 7, Plus, Num 8, ClosingBracket]
 -- という風にトークンとして解析する
@@ -30,8 +32,9 @@ exprParser = do
             mChar <- optionMaybe anyChar
             case mChar of
                 Nothing -> return () -- Parse complete
-                Just c -> parserFail $ "Unepxected character: " <> [c] -- Throw error
+                Just c  -> parserFail $ "Unepxected character: " <> [c] -- Throw error
         -- Put the result into the state, keep going
+        -- We want this to be in FILO
         Just t -> modifyState (\ls -> ls ++ [t]) >> exprParser
 
 parseToken :: Parsec String [Token] Token
@@ -40,13 +43,13 @@ parseToken = do
     case readMaybe str of
         Just n -> return $ Num n
         Nothing -> case str of
-            "+" -> return Plus
-            "-" -> return Minus
-            "*" -> return Multiply
-            "/" -> return Divide
-            "^" -> return Exponent
-            "(" -> return OpenBracket
-            ")" -> return ClosingBracket
+            "+"    -> return Plus
+            "-"    -> return Minus
+            "*"    -> return Multiply
+            "/"    -> return Divide
+            "^"    -> return Exponent
+            "("    -> return OpenBracket
+            ")"    -> return ClosingBracket
             others -> fail $ "Unexpected token: " <> others
 
 operator :: Parsec String state Char
