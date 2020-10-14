c5341eddfbfb3b022cbd0302b024863b869c4edc
Hiroto Shioi <shioihigg@gmail.com>
2020-10-15 01:14:06 +0900

    Implement command line argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,9 @@
 module Main where
 
+import CLI
+import Options.Applicative
+
 main :: IO ()
-main = undefined
+main = do
+    options <- execParser opts
+    print options
--- src/CLI.hs
+++ src/CLI.hs
@@ -1,3 +1,35 @@
 module CLI where
 
+import           Data.Text
+import           Options.Applicative (Parser, ParserInfo, fullDesc, header,
+                                      help, helper, info, long, metavar,
+                                      progDesc, short, strArgument, strOption,
+                                      (<**>), (<|>))
+
 -- コマンドラインの挙動を実装するモジュール
+
+data Input
+    = Expression Text
+    | FileInput Text
+    deriving Show
+
+fileInput :: Parser Input
+fileInput = FileInput <$> strOption
+  (  long "file"
+  <> short 'f'
+  <> metavar "FILENAME"
+  <> help "Input file" )
+
+stdInput :: Parser Input
+stdInput = Expression <$> strArgument
+  (  help "Read math expression from CLI" )
+
+
+input :: Parser Input
+input = fileInput <|> stdInput
+
+opts :: ParserInfo Input
+opts = info (input <**> helper)
+  ( fullDesc
+  <> progDesc "Calculator"
+  <> header "Calculator implemented in haskell" )
